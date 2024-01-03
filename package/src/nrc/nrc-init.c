2ed49e2e7bcce94dd96f3a4788f5198a8d34215c
Tim Harvey <tharvey@gateworks.com>
2024-01-03 11:14:40 -0800

    nrc-init: default to spi polling
    default to spi polling instead of hard coding a platform-specific GPIO
    as a module param.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- package/src/nrc/nrc-init.c
+++ package/src/nrc/nrc-init.c
@@ -85,7 +85,7 @@ int spi_cs_num;
 module_param(spi_cs_num, int, 0600);
 MODULE_PARM_DESC(spi_cs_num, "SPI chip select number");
 
-int spi_gpio_irq = 5;
+int spi_gpio_irq = -1;
 module_param(spi_gpio_irq, int, 0600);
 MODULE_PARM_DESC(spi_gpio_irq, "SPI gpio irq");
 
